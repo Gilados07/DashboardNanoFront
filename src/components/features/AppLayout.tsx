65e7f7c57d55f4756d8018ba3c0d5436c3ea59ff
Gilad Bresinski <giladbresinski@Gilads-MacBook-Pro.local>
2024-11-20 23:22:53 +0200

    Refactor Layout

--- src/components/features/AppLayout.tsx
+++ src/components/features/AppLayout.tsx
@@ -1,11 +1,11 @@
 import { ChevronDown, GalleryVerticalEnd } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import {
-  Sidebar,
+  Sidebar as BaseSideBar,
   SidebarContent,
   SidebarGroup,
   SidebarHeader,
-  SidebarInset,
+  SidebarInset as MainArea,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
@@ -29,70 +29,81 @@ interface AppLayoutProps {
 export function AppLayout({ children }: AppLayoutProps) {
   return (
     <SidebarProvider>
-      <Sidebar>
-        <SidebarHeader>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton size="lg" asChild>
-                <a href="#">
-                  <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                    <GalleryVerticalEnd className="size-4" />
-                  </div>
-                  <div className="flex flex-col gap-0.5 leading-none">
-                    <span className="font-semibold">Nano Dashboard</span>
-                    <span className="">v1.0.0</span>
-                  </div>
-                </a>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarHeader>
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarMenu>
-              {navigationList.map((item, index) => (
-                <Collapsible key={item.title} defaultOpen={index === 0}>
-                  <CollapsibleTrigger asChild>
-                    <SidebarMenuItem>
-                      <SidebarMenuButton className="w-full justify-between hover:bg-sidebar-accent/80">
-                        <span>{item.title}</span>
-                        <ChevronDown className="h-4 w-4" />
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  </CollapsibleTrigger>
-                  <CollapsibleContent>
-                    <div className="pl-4 pt-1">
-                      {item.items?.map((subItem) => (
-                        <SidebarMenuItem key={subItem.title}>
-                          <SidebarMenuButton asChild>
-                            <Link
-                              to={subItem.url}
-                              className="flex w-full items-center :focus"
-                            >
-                              {subItem.title}
-                            </Link>
-                          </SidebarMenuButton>
-                        </SidebarMenuItem>
-                      ))}
-                    </div>
-                  </CollapsibleContent>
-                </Collapsible>
-              ))}
-            </SidebarMenu>
-          </SidebarGroup>
-        </SidebarContent>
-        <SidebarRail />
-      </Sidebar>
-      <SidebarInset className="max-h-40">
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-3">
-            <SidebarTrigger />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <NavigationBreadcrumbs />
-          </div>
-        </header>
+      <Sidebar />
+      <MainArea className="max-h-40">
+        <TopBar />
         {children}
-      </SidebarInset>
+      </MainArea>
     </SidebarProvider>
   );
 }
+
+const Sidebar = () => {
+  return (
+    <BaseSideBar>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <GalleryVerticalEnd className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">Nano Dashboard</span>
+                  <span className="">v1.0.0</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {navigationList.map((item, index) => (
+              <Collapsible key={item.title} defaultOpen={index === 0}>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton className="w-full justify-between hover:bg-sidebar-accent/80">
+                      <span>{item.title}</span>
+                      <ChevronDown className="h-4 w-4" />
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  <div className="pl-4 pt-1">
+                    {item.items?.map((subItem) => (
+                      <SidebarMenuItem key={subItem.title}>
+                        <SidebarMenuButton asChild>
+                          <Link
+                            to={subItem.url}
+                            className="flex w-full items-center :focus"
+                          >
+                            {subItem.title}
+                          </Link>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    ))}
+                  </div>
+                </CollapsibleContent>
+              </Collapsible>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </BaseSideBar>
+  );
+};
+const TopBar = () => {
+  return (
+    <header className="flex h-16 shrink-0 items-center gap-2">
+      <div className="flex items-center gap-2 px-3">
+        <SidebarTrigger />
+        <Separator orientation="vertical" className="mr-2 h-4" />
+        <NavigationBreadcrumbs />
+      </div>
+    </header>
+  );
+};
