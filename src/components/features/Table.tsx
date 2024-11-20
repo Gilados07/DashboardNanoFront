6176562e0cf1909b648138145b195a82c5b94f29
Omer Bresinski <omerbres@gmail.com>
2024-11-20 23:44:09 +0200

    change

--- src/components/features/Table.tsx
+++ src/components/features/Table.tsx
@@ -31,6 +31,7 @@ const useTable = ({ data, pinnedCols }: TableProps) => {
   const columns: ColDef[] = Object.keys(data[0]).map((key, index) => ({
     field: key,
     pinned: pinnedCols?.includes(index),
+    // search goes here
     comparator: (element1, element2) => {
       if (!isNaN(+element1) && !isNaN(+element2)) {
         return +element1 - +element2;
