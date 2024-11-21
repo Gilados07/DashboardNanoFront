56bcbb433d8495f53419ebe4903a4763a95ec63b
Omer Bresinski <omerbres@gmail.com>
2024-11-21 12:31:49 +0200

    another refactor

--- src/App.tsx
+++ src/App.tsx
@@ -15,32 +15,30 @@ const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <div className="flex flex-1 flex-col gap-4 px-4">
-          <Router>
-            <AppLayout>
-              <Routes>
-                <Route index element={<Navigate to="/main/recap" replace />} />
-                <Route path="/main/recap" element={<DailyRecap />} />
-                <Route
-                  path="/daily/new-placements"
-                  element={<NewPlacementDaily />}
-                />
-                <Route
-                  path="/main/publisher-decrease"
-                  element={<PublisherDecrease />}
-                />
-                <Route
-                  path="/main/publisher-increase"
-                  element={<PublisherIncrease />}
-                />
-              </Routes>
-            </AppLayout>
-          </Router>
-        </div>
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <div className="flex flex-1 flex-col gap-4 px-4">
+        <Router>
+          <AppLayout>
+            <Routes>
+              <Route index element={<Navigate to="/main/recap" replace />} />
+              <Route path="/main/recap" element={<DailyRecap />} />
+              <Route
+                path="/daily/new-placements"
+                element={<NewPlacementDaily />}
+              />
+              <Route
+                path="/main/publisher-decrease"
+                element={<PublisherDecrease />}
+              />
+              <Route
+                path="/main/publisher-increase"
+                element={<PublisherIncrease />}
+              />
+            </Routes>
+          </AppLayout>
+        </Router>
+      </div>
+    </QueryClientProvider>
   );
 }
 
