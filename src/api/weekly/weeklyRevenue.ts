fa2a8dfa2d42bfbfa8a4ed7ad803be8416cd4548
Omer Bresinski <omerbres@gmail.com>
2024-12-02 13:50:18 +0000

    weekly-revenue

--- src/api/weekly/weeklyRevenue.ts
+++ src/api/weekly/weeklyRevenue.ts
@@ -1,5 +1,13 @@
 import { useQuery, queryOptions } from "@tanstack/react-query";
 import { fetchRequest } from "../fetchRequest";
+
+type IWeeklyRevenue = IWeeklyRevenueItem[];
+
+type IWeeklyRevenueItem = {
+  Total_Revenue: string;
+  date: string;
+};
+
 async function getWeeklyRevenue() {
   const data = await fetchRequest(
     `${import.meta.env.VITE_BACKEND_URL}/api/v1/reports/weekly/weekly-revenue`
@@ -8,7 +16,7 @@ async function getWeeklyRevenue() {
   return data;
 }
 
-export const newWeeklyRevenueQueryOptions = queryOptions({
+export const newWeeklyRevenueQueryOptions = queryOptions<IWeeklyRevenue>({
   queryKey: ["weekly", "weekly-revenue"],
   queryFn: getWeeklyRevenue,
   staleTime: 1000 * 5,
--- src/pages/weekly/WeeklyRevenue.tsx
+++ src/pages/weekly/WeeklyRevenue.tsx
@@ -1,29 +1,14 @@
-"use client";
-
-// import { TrendingUp } from "lucide-react";
-import {
-  Line,
-  LineChart,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  ResponsiveContainer,
-} from "recharts";
+import { Line, LineChart, XAxis, YAxis } from "recharts";
 import { useWeeklyRevenue } from "@/api/weekly/weeklyRevenue";
-import {
-  Card,
-  CardContent,
-  //   CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
+import { DashboardCard } from "@/components/features/DashboardCard";
+import { NoDataMessage } from "@/components/features/NoDataMessage";
 
 const chartConfig = {
   revenue: {
@@ -35,66 +20,109 @@ const chartConfig = {
 export function WeeklyRevenue() {
   const { data, isLoading } = useWeeklyRevenue();
 
-  const formatDate = (dateString: string) => {
-    const date = new Date(dateString);
-    return date.toLocaleDateString("en-US", { month: "short", day: "numeric" });
-  };
-
-  //   const formatCurrency = (value: string) => {
-  //     return new Intl.NumberFormat("en-US", {
-  //       style: "currency",
-  //       currency: "USD",
-  //       minimumFractionDigits: 2,
-  //       maximumFractionDigits: 2,
-  //     }).format(parseFloat(value));
-  //   };
-
   return isLoading ? (
     <div>Loading...</div>
+  ) : !data?.length ? (
+    <NoDataMessage />
   ) : (
-    <Card>
-      <CardHeader>
-        <CardTitle>Weekly Revenue</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <ChartContainer config={chartConfig}>
-          <ResponsiveContainer width="100%" height={300}>
+    <div className="flex flex-col gap-5 w-full h-full p-5 ">
+      <div className="flex gap-5 w-full h-min">
+        <WeeklyRecapChart data={data} />
+        <WeeklyRecapCards data={data} />
+      </div>
+    </div>
+  );
+}
+
+const WeeklyRecapCards = ({ data }: { data: Record<string, string>[] }) => {
+  const maxRevenueItem = data?.reduce(
+    (max, item) => (item.Total_Revenue > max.Total_Revenue ? item : max),
+    data[0]
+  );
+  const minRevenueItem = data?.reduce(
+    (min, item) => (item.Total_Revenue < min.Total_Revenue ? item : min),
+    data[0]
+  );
+
+  const formatCurrency = (value: string) => `$${parseFloat(value).toFixed(2)}`;
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = date.toLocaleDateString("en-US", { weekday: "long" });
+    const dayOfMonth = date.getDate();
+    const month = date.toLocaleDateString("en-US", { month: "long" });
+    const suffix =
+      dayOfMonth % 10 === 1 && dayOfMonth !== 11
+        ? "st"
+        : dayOfMonth % 10 === 2 && dayOfMonth !== 12
+        ? "nd"
+        : dayOfMonth % 10 === 3 && dayOfMonth !== 13
+        ? "rd"
+        : "th";
+    return `${day}, ${dayOfMonth}${suffix} of ${month}`;
+  };
+
+  return (
+    <div className="flex flex-col gap-5 w-full h-full">
+      <DashboardCard
+        mainContent={formatCurrency(maxRevenueItem!.Total_Revenue)}
+        subContent={formatDate(maxRevenueItem!.date)}
+        title="Best Day"
+      />
+      <DashboardCard
+        mainContent={formatCurrency(minRevenueItem!.Total_Revenue)}
+        subContent={formatDate(minRevenueItem!.date)}
+        title="Worst Day"
+      />
+    </div>
+  );
+};
+
+const WeeklyRecapChart = ({ data }: { data: Record<string, string>[] }) => {
+  const formatXaxisDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", {
+      month: "2-digit",
+      day: "2-digit",
+    });
+  };
+
+  return (
+    <div className="min-w-[473px]">
+      <Card>
+        <CardHeader>
+          <CardTitle>Weekly Revenue</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
             <LineChart data={data}>
-              <CartesianGrid vertical={false} stroke="hsl(var(--border))" />
               <XAxis
                 dataKey="date"
                 tickLine={false}
-                tickMargin={10}
                 axisLine={false}
-                tickFormatter={formatDate}
+                tickFormatter={formatXaxisDate}
               />
               <YAxis
                 tickLine={false}
                 tickMargin={10}
                 axisLine={false}
-                tickFormatter={(value) => `$${value}`}
+                tickFormatter={(value) =>
+                  value !== 0 ? `${value / 1000}k` : ""
+                }
               />
               <ChartTooltip
-                content={
-                  <ChartTooltipContent
-                    indicator="line"
-
-                    // formatValue={(value: string) => formatCurrency(value)}
-                  />
-                }
+                content={<ChartTooltipContent indicator="line" />}
               />
               <Line
                 type="monotone"
                 dataKey="Total_Revenue"
-                stroke="hsl(var(--chart-1))"
+                stroke="hsl(var(--chart-3))"
                 strokeWidth={2}
                 dot={false}
               />
             </LineChart>
-          </ResponsiveContainer>
-        </ChartContainer>
-      </CardContent>
-      <CardFooter className="flex-col items-start gap-2 text-sm"></CardFooter>
-    </Card>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+    </div>
   );
-}
+};
